829a0b17578a7a92237317b3eb981ffbc667a91c
Alex Libman <5430928+aslibman@users.noreply.github.com>
2025-09-28 20:51:46 -0400

    Enable atuin in fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,11 +1,16 @@
 # Disable greeting message
 set -g fish_greeting
 
-# Enable starship prompt
-set --export STARSHIP_CONFIG ~/.config/starship/starship.toml
-starship init fish | source
-
 function fish_postexec --on-event fish_postexec
     set -l last_status $status
     print_prompt_separator $last_status
+end
+
+if status is-interactive
+    # Enable starship prompt
+    set --export STARSHIP_CONFIG ~/.config/starship/starship.toml
+    starship init fish | source
+
+    # Enable atuin
+    atuin init fish | source
 end
