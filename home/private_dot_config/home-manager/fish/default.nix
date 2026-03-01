7976ebe4c6a4007523e60f66125e9eddf00d3365
Alex Libman <5430928+aslibman@users.noreply.github.com>
2026-03-01 16:52:20 -0500

    Migrate fish scripts to home-manager

--- home/private_dot_config/home-manager/fish/default.nix
+++ home/private_dot_config/home-manager/fish/default.nix
@@ -14,53 +14,53 @@
       # Ensure ~/.nix-profile/bin takes priority over system paths.
       # fish_user_paths is always prepended to $PATH by fish.
       fish_add_path --move --prepend "$HOME/.nix-profile/bin"
+    '';
 
-      function fish_postexec --on-event fish_postexec
+    interactiveShellInit = builtins.readFile ./interactive.fish;
+
+    shellAbbrs = {
+      shellcheck = "shellcheck --color";
+      "--help" = {
+        expansion = "--help | bat -plhelp";
+        position = "anywhere";
+      };
+      "-h" = {
+        expansion = "-h | bat -plhelp";
+        position = "anywhere";
+      };
+    };
+
+    shellAliases = {
+      vim = "nvim";
+      ls = "eza";
+      find = "fd";
+      ps = "procs";
+      gamend = "git commit --amend --no-edit";
+    };
+
+    functions = {
+      fish_postexec = {
+        onEvent = "fish_postexec";
+        body = ''
           set -l last_status $status
           print_prompt_separator $last_status
-      end
-    '';
+        '';
+      };
 
-    interactiveShellInit = ''
-      # Enable vi mode
-      fish_vi_key_bindings
+      print_prompt_separator.body = builtins.readFile ./print_prompt_separator.fish;
 
-      # Add custom bin scripts to PATH
-      fish_add_path "$HOME/.local/bin"
+      __atuin_setup = {
+        description = "Initialize atuin + fzf history widget for fish";
+        body = builtins.readFile ./atuin_setup.fish;
+      };
 
-      # Enable starship prompt
-      set --export STARSHIP_CONFIG ~/.config/starship/starship.toml
-      starship init fish | source
-
-      # Set up fzf key bindings
-      fzf --fish | source
-
-      # Preview file content using bat (https://github.com/sharkdp/bat)
-      set --export FZF_CTRL_T_OPTS "
-        --preview 'bat -n --color=always {}'
-        --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-
-      # Dracula FZF theme: https://draculatheme.com/fzf
-      set --export FZF_DEFAULT_OPTS '--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
-
-      # Load atuin -- must run after fzf to override its CTRL-R binding
-      # See https://github.com/junegunn/fzf/issues/4417
-      __atuin_setup
-
-      # Use bat for man pages - https://github.com/sharkdp/bat#man
-      batman --export-env | source
-
-      # Setup source-highlight for less pager
-      set --export LESSOPEN "| $HOME/.nix-profile/bin/src-hilite-lesspipe.sh %s"
-      set --export LESS -R
-
-      # Setup keychain for SSH key management
-      if type -q keychain
-          keychain --eval --quiet --agents ssh id_rsa id_ed25519 2>/dev/null
-          if test -f ~/.keychain/(hostname)-fish
-              source ~/.keychain/(hostname)-fish
-          end
-      end
-    '';
+      ff-widget.body = ''
+        set -l selected (ff)
+        if test -n "$selected"
+            commandline -i -- $selected
+        end
+        commandline -f repaint
+      '';
+    };
   };
 }
---
+++ home/private_dot_config/home-manager/fish/print_prompt_separator.fish
@@ -0,0 +1,55 @@
+set -l last_status $argv[1]  # receive status from fish_postexec
+
+set dot "·"
+set color (set_color -o brblack)     # Light gray dots
+set timecolor (set_color brcyan)     # Datetime color
+set rtcolor (set_color bryellow)     # Runtime color
+set errcolor (set_color red)         # Error code color
+set reset (set_color normal)
+
+# Terminal width
+set cols (tput cols)
+
+# Current date and time
+set datetime (date "+%Y-%m-%d %H:%M:%S")
+
+# Command duration formatting (only if >= 2s)
+set runtime ""
+if test $CMD_DURATION -ge 2000
+    set total_secs (math --scale=0 "$CMD_DURATION / 1000")
+
+    set hours (math --scale=0 "$total_secs / 3600")
+    set mins (math --scale=0 "($total_secs % 3600) / 60")
+    set secs (math --scale=0 "$total_secs % 60")
+
+    set formatted_runtime ""
+    if test $hours -gt 0
+        set formatted_runtime $hours"h "$mins"m "$secs"s"
+    else if test $mins -gt 0
+        set formatted_runtime $mins"m "$secs"s"
+    else
+        set formatted_runtime $secs"s"
+    end
+
+    set runtime " | $rtcolor$formatted_runtime$reset"
+end
+
+# Status code (only show if non-zero)
+set status_text ""
+if test $last_status -ne 0
+    set status_text " | $errcolor✖ Exit $last_status$reset"
+end
+
+# Center text: datetime + runtime (if any)
+set center_text "$timecolor$datetime$reset$runtime$status_text"
+
+# Compute left/right padding
+set text_len (string length --visible $center_text)
+set left_len (math --scale=0 "($cols - $text_len) / 2")
+set right_len (math --scale=0 "$cols - $left_len - $text_len")
+
+set left (string repeat -n $left_len $dot)
+set right (string repeat -n $right_len $dot)
+
+# Print full-width dotted line with centered info
+printf "\n$color$left$reset$center_text$color$right$reset\n"
