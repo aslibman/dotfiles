e409d14a6a7f09db239c12cedede32c7135ab8f6
Alex Libman <5430928+aslibman@users.noreply.github.com>
2026-03-13 20:17:46 -0400

    Add nix formatting pre-commit hook

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Home-manager dotfiles";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nixfmt-tree
+              pkgs.prek
+            ];
+            shellHook = ''
+              prek install
+              prek install-hooks
+            '';
+          };
+        }
+      );
+    };
+}
