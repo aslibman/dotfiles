067be050eaf0c008e8b8269426c33a12575605cc
Alex Libman <5430928+aslibman@users.noreply.github.com>
2026-03-17 19:49:35 -0400

    Disable iterm2 auto-update

--- home/private_dot_config/home-manager/iterm2/default.nix
+++ home/private_dot_config/home-manager/iterm2/default.nix
@@ -6,6 +6,10 @@
     nerd-fonts.inconsolata
   ];
 
+  targets.darwin.defaults."com.googlecode.iterm2" = {
+    SUEnableAutomaticChecks = false;
+  };
+
   home.file = {
     "Library/Application Support/iTerm2/DynamicProfiles/profiles.json".source = ./profiles.json;
     "Library/Application Support/iTerm2/ColorPresets/Dracula.itermcolors".source =
